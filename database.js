d7dbed1406c6544ba4e24a04eee32d2c057e1d68
DishaRabadiya <126776529+DishaRabadiya@users.noreply.github.com>
2025-02-13 18:28:00 +0530

    Update database.js

--- database.js
+++ database.js
@@ -14,3 +14,39 @@ const DBconnection = async () => {
 };
 
 module.exports = DBconnection;
+
+
+//  ----------- .env.local --------
+// PORT=5005
+// DATABASE_NAME='chat-app'
+// # CONNECTION_STRING="mongodb://192.168.43.141:27017/"
+// CONNECTION_STRING="mongodb://0.0.0.0:27017/"
+
+// --------- .gitignore ----------
+// # See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+// # dependencies
+// /node_modules
+// /.pnp
+// /logs
+// .pnp.js
+// /public
+// # testing
+// /coverage
+// # production
+// /build
+// # misc
+// .DS_Store
+// .env.local
+// .env.development.local
+// .env.test.local
+// .env.production.local
+
+// npm-debug.log*
+// yarn-debug.log*
+// yarn-error.log*
+// package-lock.json
+// .docz
+// .eslintcache
+// .vscode
+// /logs/*
